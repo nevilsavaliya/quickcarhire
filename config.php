db86938be763215414e9ebea14f657b85764615f
Savaliya Nevil <savaliyanevil9@gmail.com>
2024-03-11 18:21:10 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
-$google_client->setClientId('670885709604-8idtbvtpioqfeq3m9s274dh8t8u77h90.apps.googleusercontent.com');
-$google_client->setClientSecret('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');
+$google_client->setClientId('457578089846-gomi9ia6h27k24ie2fet2fd4m5f24p7l.apps.googleusercontent.com');
+$google_client->setClientSecret('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');
 $google_client->setRedirectUri('http://quickcarhire.live/check.php');
 $google_client->addScope('email');
 $google_client->addScope('profile');
